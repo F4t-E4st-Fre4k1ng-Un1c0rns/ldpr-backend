0e959b99f551ff1a89d6f8464e5bf094d8782ed2
matv864 <matv864@gmail.com>
2025-04-19 21:56:23 +1000

    clean

--- src/admin/admin_models/__init__.py
+++ src/admin/admin_models/__init__.py
@@ -1,10 +1,6 @@
 # порядок импортов здесь - это порядок списка моделей
-
-# управление менеджерами
+import src.admin.admin_models.manager
+import src.admin.admin_models.client
 import src.admin.admin_models.anime
 import src.admin.admin_models.season
 import src.admin.admin_models.episode
-import src.admin.admin_models.client
-import src.admin.admin_models.email_template
-import src.admin.admin_models.manager
-import src.admin.admin_models.news
